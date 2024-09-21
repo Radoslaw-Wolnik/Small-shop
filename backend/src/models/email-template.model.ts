3812a9b1fcb4d14a1bf9ad6cdc53610368510f3b
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-09-21 19:07:13 +0100

    added email-template model

---
+++ backend/src/models/email-template.model.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IEmailTemplateDocument extends Document {
+  name: string;
+  subject: string;
+  body: string;
+  variables: string[];
+}
+  
+const emailTemplateSchema = new Schema<IEmailTemplateDocument>({
+  name: { type: String, required: true, unique: true },
+  subject: { type: String, required: true },
+  body: { type: String, required: true },
+  variables: [{ type: String }],
+});
+
+export const EmailTemplate = mongoose.model<IEmailTemplateDocument>('EmailTemplate', emailTemplateSchema);
