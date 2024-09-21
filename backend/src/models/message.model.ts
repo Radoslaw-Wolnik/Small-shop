d28215d7e1512ccf2aaa0f35360d511f1c7471d1
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-09-21 19:09:38 +0100

    now users or not logged in - dispiutes by magic links - can send messages (create dispiutes

--- backend/src/models/message.model.ts
+++ backend/src/models/message.model.ts
@@ -2,7 +2,8 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IMessageDocument extends Document {
-  sender: Schema.Types.ObjectId;
+  sender?: Schema.Types.ObjectId;
+  customerEmail?: string;
   content: string;
   readStatus: boolean;
   category: 'dispute' | 'contact' | 'other';
@@ -13,11 +14,29 @@ export interface IMessageDocument extends Document {
 
 const messageSchema = new Schema<IMessageDocument>({
   sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  customerEmail: { 
+    type: String,
+    validate: {
+      validator: function(v: string) {
+        return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: 'Please enter a valid email'
+    }
+  },
   content: { type: String, required: true },
   readStatus: { type: Boolean, default: false },
   category: { type: String, enum: ['dispute', 'contact', 'other'], required: true },
   relatedOrder: { type: Schema.Types.ObjectId, ref: 'Order' },
   relatedDispute: { type: Schema.Types.ObjectId, ref: 'Dispute' },
 }, { timestamps: true });
+
+// Custom validator to ensure either sender or customerEmail is provided
+messageSchema.pre('validate', function(next) {
+  if (!this.sender && !this.customerEmail) {
+    next(new Error('Either sender or customerEmail must be provided'));
+  } else {
+    next();
+  }
+});
 
 export const Message = mongoose.model<IMessageDocument>('Message', messageSchema);
