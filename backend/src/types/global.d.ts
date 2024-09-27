89693d877f2b47b4b928bb4648d881f837d5885f
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-09-27 17:14:08 +0200

    moved types global to src (finally)

--- backend/src/types/global.d.ts
+++ backend/src/types/global.d.ts
@@ -2,7 +2,7 @@
 
 import { Request } from 'express';
 import { Types } from 'mongoose';
-import { IUserDocument } from '../src/models/user.model';
+import { IUserDocument } from '../models/user.model';
 
 
 declare global {
